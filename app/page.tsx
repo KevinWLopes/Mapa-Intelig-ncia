8f91e08b31c07ff3fce1b2d43e370f95ccdabef8
KevinWLopes <153891069+KevinWLopes@users.noreply.github.com>
2026-03-30 21:47:09 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Mapa de Inteligência 🚔</h1>
+      <h1>Mapa de Inteligência 🚓</h1>
       <p>Sistema rodando 🔥</p>
     </div>
   );
